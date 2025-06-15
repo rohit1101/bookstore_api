cad02b784cd0d43c7421da38fc3eca463bdfba7a
rohit1101 <srohit1101@gmail.com>
2025-06-15 16:16:12 +0530

    add put and delete routes

--- main.py
+++ main.py
@@ -15,11 +15,17 @@ async def get_all_books():
   return "all books"
 
 @app.get('/api/v1/books/{book_id}')
-async def get_book_by_id(book_id:int=0):
+async def get_book_by_id(book_id:int):
   return f'The book ID is {book_id}'
 
 @app.post('/api/v1/books/')
 async def add_book():
   return f'Book added'
 
+@app.put('/api/v1/books/{book_id}')
+async def update_book(book_id:int):
+  return f'Updated book {book_id}'
 
+@app.delete('/api/v1/books/{book_id}')
+async def delete_book(book_id:int):
+  return f'Delete book {book_id}'
