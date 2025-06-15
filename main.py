a57416da4d621552a1b1d047810182b39d317a43
rohit1101 <srohit1101@gmail.com>
2025-06-15 15:54:45 +0530

    add get routes

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def healthcheck():
 async def get_all_books():
   return "all books"
 
-@app.post('/api/v1/books/{id}')
-async def get_book_by_id():
-  return "get book by ID"
+@app.get('/api/v1/books/{book_id}')
+async def get_book_by_id(book_id:int=0):
+  return f'The book ID is {book_id}'
+
 
